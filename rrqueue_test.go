03075f49eb486446a56b7c4342b7dea9c02dc6db
Burcu Dogan <burcud@google.com>
2013-09-26 15:50:39 +0200

    Add tests and travis build.

--- rrqueue_test.go
+++ rrqueue_test.go
@@ -13,3 +13,35 @@
 // limitations under the License.
 
 package rrqueue
+
+import (
+	"testing"
+)
+
+func TestNumPriorityQueues(t *testing.T) {
+	q := New(4)
+	if len(q.queues) != 4 {
+		t.Error("New: can't create correct number of priority queues")
+	}
+}
+
+func TestEnqueue(t *testing.T) {
+	q := New(5)
+	q.Enqueue(3, "item")
+
+	if q.queues[3][0] != "item" {
+		t.Error("Enqueue: can't put the item to the right queue")
+	}
+}
+
+func TestDequeue(t *testing.T) {
+	q := New(5)
+	q.Enqueue(0, "P0 item")
+	q.Enqueue(1, "P1 item")
+	q.Enqueue(1, "Another P1 item")
+
+	item, err := q.Dequeue(1)
+	if err != nil || item != "P1 item" {
+		t.Error("Dequeue: can't dequeue the right element")
+	}
+}
